cc865c3a0d50d8fabcd7171146f0a578f22cbd20
lils-it <lil-it@gmx.de>
2024-08-24 20:22:13 +0400

    homeWork5Brace

---
+++ src/homework4plus5/Brace.java
@@ -0,0 +1,18 @@
+package homework4plus5;
+
+public class Brace {
+
+    char brace;
+    int index;
+
+
+    public  Brace (char brace, int index) {
+        this.brace = brace;
+        this.index = index;
+    }
+
+    @Override
+    public String toString() {
+        return  brace + " " ;
+    }
+}
